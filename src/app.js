d062784f892376f79100282afb10420e4728d83b
Arya <aryajha195@gmail.com>
2024-11-30 12:22:50 +0530

    exploring http calls

--- src/app.js
+++ src/app.js
@@ -2,13 +2,26 @@ const express = require('express');
 
 const app = express();
 
-app.use("/hello",(req, res) => {
-    res.send("Hello Hello Hello")
+app.get("/user", (req, res) => {
+    res.send({
+        firstName: "Arya",
+        lastName: "Jha"
+    })
+})
+
+app.post("/user", (req, res) => {
+    res.send("User Saved Successfully!")
+})
+
+app.delete("/user", (req, res) => {
+    res.send("User Deleted Successfully!")
 })
 
 app.use("/",(req, res) => {
     res.send("Hello from the server")
 })
+
+
 
 app.listen(3000, () => {
     console.log("Server is successfully listening on port 3000...")
